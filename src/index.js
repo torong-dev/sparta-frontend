e9a431620ce52383e43c699be216cbcac2de5e3d
torong <hi_ryuji@naver.com>
2024-01-20 03:36:41 +0900

    :art: Create project structure
    페이지를 구성하는 폴더 및 파일 생성. index.js, index.css, App.js 수정

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import Signup from "./pages/Signup/Signup.jsx";
+import Login from "./pages/Login/Login.jsx";
+import NotFound from "./pages/NotFound/NotFound.jsx";
+import Catalog from "./pages/Catalog/Catalog.jsx";
+import Classroom from "./pages/Classroom/Classroom.jsx";
+import Enrolleds from "./pages/Enrolleds/Enrolleds.jsx";
+import Details from "./pages/Details/Details.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: "/", element: <Home /> },
+      { path: "/signup", element: <Signup /> },
+      { path: "/login", element: <Login /> },
+      { path: "/catalog", element: <Catalog /> },
+      { path: "/classroom", element: <Classroom /> },
+      { path: "/enrolleds", element: <Enrolleds /> },
+      { path: "/details", element: <Details /> },
+    ],
+  },
+]);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
+
 reportWebVitals();
