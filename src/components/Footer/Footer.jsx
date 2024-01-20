0947b30f5d369f80c405da02d0b75a2a5d9fddd3
torong <hi_ryuji@naver.com>
2024-01-21 02:10:30 +0900

    :recycle: Refactor Navbar
    Navbar 리팩토링

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,45 @@
 import React from "react";
+import "../Footer/index.css";
 
 export default function Footer() {
-  return <div>Footer Page</div>;
+  return (
+    <footer className="footer__section">
+      <footer className="footer__section__container">
+        <div className="footer__section__menu">
+          <div className="footer__section__customer">
+            <div className="footer__section__title">고객센터</div>
+            <div>
+              <div>홈페이지 우측 하단 [문의하기]를 통해</div>
+              <div>1:1 채팅상담을 이용하실 수 있습니다.</div>
+            </div>
+          </div>
+          <div className="footer__section__service">
+            <div className="footer__section__title">서비스</div>
+          </div>
+          <div className="footer__section__family">
+            <div className="footer__section__title">패밀리 사이트</div>
+          </div>
+          <div className="footer__section__corp">
+            <div className="footer__section__title">기업 서비스</div>
+          </div>
+          <div className="footer__section__company">
+            <div className="footer__section__title">회사</div>
+          </div>
+        </div>
+        <div className="footer__contents">
+          <ul className="footer__contents__bar">
+            <div></div>
+            <div></div>
+          </ul>
+          <div className="footer__contents__info">
+            <section className="footer__contents__section">
+              <div className="footer__contents__title"></div>
+              <div className="footer__contents__etc"></div>
+            </section>
+            <p className="footer__contents__c"></p>
+          </div>
+        </div>
+      </footer>
+    </footer>
+  );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,30 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FaAngleDown } from "react-icons/fa";
-import { FaAngleUp } from "react-icons/fa";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 import "../Navbar/index.css";
 
-export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isIconUp, setIsIconUp] = useState(false);
+// 메뉴 버튼에 대한 컴포넌트
+const NavbarBtn = ({ to, children }) => (
+  <Link to={to}>
+    <div className="navbar__menu__btn">{children}</div>
+  </Link>
+);
 
+// Navbar 컴포넌트 정의
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // 드롭다운 토글 함수
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
-    setIsIconUp(!isIconUp);
   };
+
+  // 드롭다운 메뉴 항목
+  const dropdownContents = [
+    { text: "기업 교육", link: "/" },
+    { text: "외주 개발", link: "/" },
+    { text: "신입 개발자 채용", link: "/" },
+  ];
 
   return (
     <div className="navbar__section">
@@ -23,63 +36,56 @@ export default function Navbar() {
           <Link to="/">
             <div className="navbar__services__center__btn">고객센터</div>
           </Link>
-          <div>
-            <div className="navbar__services_crp">
-              <button
-                onClick={toggleDropdown}
-                className="navbar__services__crp__btn"
-              >
-                기업 서비스&nbsp;{isIconUp ? <FaAngleUp /> : <FaAngleDown />}
-              </button>
-              {isOpen && (
-                <div className="navbar__toggle-dropdown">
-                  <a href="/" className="navbar__toggle-dropdown__contents">
-                    기업 교육
-                  </a>
-                  <div className="navbar__toggle-dropdown__line"></div>
-                  <a href="/" className="navbar__toggle-dropdown__contents">
-                    외주 개발
-                  </a>
-                  <div className="navbar__toggle-dropdown__line"></div>
-                  <a href="/" className="navbar__toggle-dropdown__contents">
-                    신입 개발자 채용
-                  </a>
-                </div>
-              )}
-            </div>
+          <div className="navbar__services_corp">
+            <button
+              onClick={toggleDropdown}
+              className={`navbar__services__corp__btn ${isOpen ? "up" : "down"}`}
+            >
+              기업 서비스&nbsp;{isOpen ? <FaAngleUp /> : <FaAngleDown />}
+            </button>
+            {isOpen && (
+              <div className="navbar__toggle-dropdown">
+                {dropdownContents.map(({ text, link }, index) => (
+                  <React.Fragment key={index}>
+                    <a
+                      href={link}
+                      className="navbar__toggle-dropdown__contents"
+                    >
+                      {text}
+                    </a>
+                    {/* 드롭다운 메뉴 항목들 사이에 선 추가, 마지막 항목에는 선을 추가하지 않음 */}
+                    {index < dropdownContents.length - 1 && (
+                      <div className="navbar__toggle-dropdown__line"></div>
+                    )}
+                  </React.Fragment>
+                ))}
+              </div>
+            )}
           </div>
         </div>
       </div>
-      <div className="navbar__line"></div>
+      <div className="navbar__section__line"></div>
       <div className="navbar__menu">
         <div className="navbar__menu__items">
           <Link to="/">
             <img
-              className="navbar__logo"
+              className="navbar__menu__logo"
               src="imgs/sparta-navbar-logo.png"
               alt="logo"
             />
           </Link>
-          <Link to="/catalog">
-            <div className="navbar__menu__catalog__btn">전체 강의</div>
-          </Link>
-          <Link to="/">
-            <div className="navbar__menu__btn">
-              부트캠프 <div className="navbar__menu__btn__txt">국비</div>
-            </div>
-          </Link>
-          <Link to="/">
-            <div className="navbar__menu__btn">커뮤니티</div>
-          </Link>
-          <Link to="/">
-            <div className="navbar__menu__btn">블로그</div>
-          </Link>
-          <Link to="/">
-            <div className="navbar__menu__btn">이벤트</div>
-          </Link>
+          <NavbarBtn to="/catalog">전체 강의</NavbarBtn>
+          <NavbarBtn to="/">
+            부트캠프 <div className="navbar__menu__btn__txt">국비</div>
+          </NavbarBtn>
+          <NavbarBtn to="/">커뮤니티</NavbarBtn>
+          <NavbarBtn to="/">블로그</NavbarBtn>
+          <NavbarBtn to="/">이벤트</NavbarBtn>
         </div>
-        <button className="navbar__login__btn">로그인</button>
+        <button className="navbar__menu__login__btn">로그인</button>
       </div>
     </div>
   );
-}
+};
+
+export default Navbar;
